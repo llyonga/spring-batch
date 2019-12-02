1932accbdca35936e8e59a1193185167a7a831fa
llyong <>
2019-12-02 22:05:47 +0800

    flow step demo

--- src/main/java/cn/llyong/batch/config/JobDemoFlow.java
+++ src/main/java/cn/llyong/batch/config/JobDemoFlow.java
@@ -6,6 +6,8 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.Flow;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -16,13 +18,13 @@ import org.springframework.context.annotation.Configuration;
 /**
  * @description:
  * @author: llyong
- * @date: 2019/11/30
- * @time: 15:24
+ * @date: 2019/12/2
+ * @time: 21:32
  * @version: 1.0
  */
 @Configuration
 @EnableBatchProcessing
-public class JobStepsDemo {
+public class JobDemoFlow {
 
     @Autowired
     private JobBuilderFactory jobBuilderFactory;
@@ -31,34 +33,52 @@ public class JobStepsDemo {
     private StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job jobDemo() {
-        return jobBuilderFactory.get("jobDemo")
-                .start(step1())
-                .next(step2())
+    public Step flowStep1() {
+        return stepBuilderFactory.get("flowStep1").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("execute flowStep1....");
+                return RepeatStatus.FINISHED;
+            }
+       }).build();
+    }
+
+    @Bean
+    public Step flowStep2() {
+        return stepBuilderFactory.get("flowStep2").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("execute flowStep2....");
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
+
+    @Bean
+    public Step flowStep3() {
+        return stepBuilderFactory.get("flowStep3").tasklet(new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("execute flowStep3....");
+                return RepeatStatus.FINISHED;
+            }
+        }).build();
+    }
+
+    /**
+     * 指明flow对象包含哪些step
+     * @return
+     */
+    @Bean
+    public Flow flowDemo1() {
+        return new FlowBuilder<Flow>("flowDemo1")
+                .start(flowStep1()).next(flowStep2()).build();
+    }
+
+    @Bean
+    public Job jobFlowDemo() {
+        return jobBuilderFactory.get("jobFlowDemo")
+                .start(flowDemo1()).next(flowStep3()).end()
                 .build();
     }
-
-    @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2").tasklet(new Tasklet() {
-            @Override
-            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                System.out.println("execute step2......");
-                return RepeatStatus.FINISHED;
-            }
-        }).build();
-    }
-
-    @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1").tasklet(new Tasklet() {
-            @Override
-            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                System.out.println("execute step1......");
-                return RepeatStatus.FINISHED;
-            }
-        }).build();
-    }
-
-
 }
